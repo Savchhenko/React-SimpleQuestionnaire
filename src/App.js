c697bf4dd100f01bcc56a0feca9b815989ac1a0e
Alexandra Savchenko <svchhh@yandex.ru>
2022-11-26 23:29:27 +0300

    save input's data to LocalStorage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
 
-import questionsList from "./questionsList.json";
+import questionsData from "./questionsList.json";
 import QABlock from "./components/qa-block";
 
 function App() {
-    const [answers, setAnswers] = useState(null);
+    const [questionsList, setQuestionsList] = useState(null);
+    const [answers, setAnswers] = useState([]);
+    let inputElems = []; //the variable for all form's input elements
 
     useEffect(() => {
-        setAnswers(questionsList[0]);
-        console.log(answers);
+        setQuestionsList(questionsData[0]); //save JSON data for rendering questions
     }, []);
 
-    const { questions } = answers ?? {};
+    const { questions } = questionsList ?? {};
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log("Btn was clicked");
+        inputElems = document.querySelectorAll(".answer-input");
+        console.log('Submit button was clicked');
+        [...inputElems].forEach((elem) => {
+            setAnswers(prevValues => [...prevValues, {
+                id: prevValues.length + 1,
+                answer: elem.value
+            }]);
+        });
     };
+    
+    useEffect(() => {
+        console.log("User answers: ", answers);
+        localStorage.setItem('answers', JSON.stringify(answers));
+    }, [answers]);
+
     return (
         <main>
             <form className="main-container" onSubmit={handleSubmit}>
